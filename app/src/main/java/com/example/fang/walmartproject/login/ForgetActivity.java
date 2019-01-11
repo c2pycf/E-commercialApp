3959e75abc98c85621a5fe117a497011383c13d2
Fang Chen <c2pycf@gmail.com>
2019-01-12 07:58:39 +0900

    Update - forget

---
+++ app/src/main/java/com/example/fang/walmartproject/login/ForgetActivity.java
@@ -0,0 +1,13 @@
+package com.example.fang.walmartproject.login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+public class ForgetActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
